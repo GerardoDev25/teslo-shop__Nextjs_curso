06036bd65ada4109efbc340cef13929aea4ea3fe
Gerardo Miranda <gerardodev25@gmail.com>
2023-02-04 12:22:51 -0400

    6. Estructura de directorios y ShopLayout

---
+++ components/layout/ShopLayout.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import { FC, ReactNode } from 'react';
+
+interface Props {
+  title: string;
+  pageDescription: string;
+  imageFullUrl?: string;
+  children: ReactNode;
+}
+
+export const ShopLayout: FC<Props> = (props) => {
+  const { children, pageDescription, title, imageFullUrl } = props;
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name='description' content={pageDescription} />
+        <meta name='og:title' content={title} />
+        <meta name='og:description' content={pageDescription} />
+        {imageFullUrl && <meta name='og:image' content={imageFullUrl} />}
+      </Head>
+
+      <nav>{/* todo: nanbar  */}</nav>
+
+      {/* todo sidebar */}
+
+      <main style={{ margin: '80px auto', maxWidth: '1440px', padding: '0 30px' }}>{children}</main>
+      <footer>{/* todo custom footer */}</footer>
+    </>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
+import { Typography } from '@mui/material';
+import { ShopLayout } from '@/components/layout';
+
 function TesloShop() {
   return (
-    <>
-      <h1>Teslo-shop</h1>
-    </>
+    <ShopLayout title={'Teslo-Shop'} pageDescription={'encuentra los mujores productos aqui'}>
+      <Typography variant='h1' component='h1'>
+        Teslo-shop
+      </Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>
+        Todos los productos
+      </Typography>
+    </ShopLayout>
   );
 }
 export default TesloShop;

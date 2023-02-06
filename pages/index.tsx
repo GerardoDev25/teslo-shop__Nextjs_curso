e932649c7bc4050f2bfb2b238883034ffaebc691
Gerardo Miranda <gerardodev25@gmail.com>
2023-02-05 21:30:36 -0400

    9. Home Page - Temporal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,33 @@
-import { Typography } from '@mui/material';
+import { CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
 import { ShopLayout } from '@/components/layout';
+import { initialData } from '@/database/products';
 
 function TesloShop() {
   return (
-    <ShopLayout title={'Teslo-Shop'} pageDescription={'encuentra los mujores productos aqui'}>
+    <ShopLayout
+      title={'Teslo-Shop'}
+      pageDescription={'encuentra los mujores productos aqui'}
+    >
       <Typography variant='h1' component='h1'>
         Teslo-shop
       </Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>
         Todos los productos
       </Typography>
+
+      <Grid container spacing={4}>
+        {initialData.products.map((product) => (
+          <Grid item xs={6} sm={4} key={product.slug}>
+            <CardActionArea>
+              <CardMedia
+                component={'img'}
+                image={`products/${product.images[0]}`}
+                alt={product.title}
+              />
+            </CardActionArea>
+          </Grid>
+        ))}
+      </Grid>
     </ShopLayout>
   );
 }

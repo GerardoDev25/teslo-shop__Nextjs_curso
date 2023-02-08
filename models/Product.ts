755391ade8bf335895c97dde640579a7e6e25a8f
Gerardo Miranda <gerardodev25@gmail.com>
2023-02-08 12:53:19 -0400

    6. Mongoose y  Modelo de Producto

---
+++ models/Product.ts
@@ -0,0 +1,43 @@
+import { IProduct } from '@/interfaces';
+import mongoose, { Schema, Model, model } from 'mongoose';
+
+const productSchemal = new Schema(
+  {
+    description: { type: String, required: true },
+    images: [{ type: String }],
+    inStock: { type: Number, required: true, default: 0 },
+    price: { type: Number, required: true, default: 0 },
+    sizes: [
+      {
+        type: String,
+        enum: {
+          values: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+          message: '{VALUE} no es un tamaña permitido',
+        },
+      },
+    ],
+    slug: { type: String, required: true, unique: true },
+    tags: [{ type: String }],
+    title: { type: String, required: true },
+    type: {
+      type: String,
+      enum: {
+        values: ['shirts', 'pants', 'hoodies', 'hats'],
+        message: '{VALUE} no es un tipo permitido',
+      },
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ['men', 'women', 'kid', 'unisex'],
+        message: '{VALUE} no es un genero permitido',
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const Product: Model<IProduct> =
+  mongoose.models.Product || model('Product', productSchemal);
+
+export default Product;

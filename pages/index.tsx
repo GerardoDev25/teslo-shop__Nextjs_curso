5cad1b687144d52b5dfea037dc39f6cc1097f6ba
Gerardo Miranda <gerardodev25@gmail.com>
2023-02-09 12:34:54 -0400

    5. SWR - React Hooks for Data Fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,26 @@
-import { CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { ShopLayout } from '@/components/layout';
 import { initialData } from '@/database/products';
 import { ProductList } from '@/components/products';
+import { NextPage } from 'next';
 
-function TesloShop() {
+import useSWR from 'swr';
+
+const fetcher = (...args: [key: string]) =>
+  fetch(...args).then((res) => res.json());
+
+const HomePage: NextPage = () => {
+  const { data, error, isLoading } = useSWR('/api/products/', fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  console.log(data);
+
   return (
     <ShopLayout
-      title={'Teslo-Shop'}
-      pageDescription={'encuentra los mujores productos aqui'}
+      title='Teslo-Shop'
+      pageDescription='encuentra los mujores productos aqui'
     >
       <Typography variant='h1' component='h1'>
         Teslo-shop
@@ -16,8 +29,8 @@ function TesloShop() {
         Todos los productos
       </Typography>
 
-      <ProductList products={initialData.products as any} />
+      <ProductList products={data} />
     </ShopLayout>
   );
-}
-export default TesloShop;
+};
+export default HomePage;

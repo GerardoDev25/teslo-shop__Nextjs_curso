5e27f96a54a28ceb2d54c184ac61d1656c59b98b
Gerardo Miranda <gerardodev25@gmail.com>
2023-03-02 11:25:47 -0400

    6. Configuración de NextAuth + GitHub

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,1 +1,15 @@
-export const aa = (e: any) => {};
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
+    }),
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);

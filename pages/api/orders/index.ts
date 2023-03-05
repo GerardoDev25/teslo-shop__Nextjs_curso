d71ac787e3168c271627b7c31f8ff6787dc59c55
Gerardo Miranda <gerardodev25@gmail.com>
2023-03-05 11:08:00 -0400

    9. Crear orden - Backend

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -1,5 +1,8 @@
+import { db } from '@/database';
 import { IOrder } from '@/interfaces';
+import { ProductModel } from '@/models';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
 
 type Data =
   | {
@@ -19,8 +22,33 @@ export default function handle(
       return res.status(400).json({ message: 'Bad Request' });
   }
 }
-const createOrder = (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  const body = req.body as IOrder;
 
-  res.status(201).json(body);
+const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { orderItems, total } = req.body as IOrder;
+  const session: any = await getSession({ req });
+
+  if (!session)
+    return res
+      .status(401)
+      .json({ message: 'Debe de estar autenticado para hacer esto' });
+
+  const productsIds = orderItems.map((p) => p._id);
+
+  await db.connect();
+  const dbProducts = await ProductModel.find({ _id: { $in: productsIds } });
+
+  try {
+    const subTotal = orderItems.reduce((prev, curr) => {
+      const currentPrice = dbProducts.find((prod) => {
+        prod._id === curr._id;
+      })?.price;
+
+      if (!currentPrice) {
+        throw new Error('Verifique el carrito de nuevo');
+      }
+      return currentPrice * curr.price + prev;
+    }, 0);
+  } catch (error) {}
+
+  res.status(201).json(req.body);
 };

be279560b91339d8e2d23c3de51f89f3ff00fff3
Gerardo Miranda <gerardodev25@gmail.com>
2023-03-22 11:01:27 -0400

    9. Paypal OAuth Token

--- pages/api/orders/pay.ts
+++ pages/api/orders/pay.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
@@ -17,6 +18,45 @@ export default function handle(
       return res.status(400).json({ message: 'bad request' });
   }
 }
-const payOrder = (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  return res.status(200).json({ message: 'orden pagada' });
+
+const getPaypalBeaterToken = async (): Promise<string | null> => {
+  const PAYPAL_CLIENT = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID;
+  const PAYPAL_SECRET = process.env.PAYPAL_SECRET_ID;
+
+  const base64Token = Buffer.from(
+    `${PAYPAL_CLIENT}:${PAYPAL_SECRET}`,
+    'utf-8'
+  ).toString('base64');
+
+  const body = new URLSearchParams('grant_type=client_credentials');
+
+  try {
+    const { data } = await axios.post(
+      process.env.PAYPAL_OAUTH_URL || '',
+      body,
+      {
+        headers: {
+          'Content-Type': 'application/x-form-urlencoded',
+          'Authorization': `Basic ${base64Token}`,
+        },
+      }
+    );
+
+    return data.access_token;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log(error.response?.data);
+    } else console.log(error);
+    return null;
+  }
+};
+
+const payOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const paypalBeaterToken = await getPaypalBeaterToken();
+
+  if (!paypalBeaterToken) {
+    return res.status(400).json({ message: 'no se pudo confirmar el token' });
+  }
+
+  return res.status(200).json({ message: paypalBeaterToken });
 };
